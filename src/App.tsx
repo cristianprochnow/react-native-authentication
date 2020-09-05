dafab7c0df7fea88f41813f3fa42f993595b0283
cristianprochnow <cristianprochnow@gmail.com>
2020-09-05 16:52:43 -0300

    chore: add default React Navigation setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
 import {View} from 'react-native';
 
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <NavigationContainer>
+      <View />
+    </NavigationContainer>
+  );
 };
 
 export default App;
